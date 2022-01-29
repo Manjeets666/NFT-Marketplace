8f883719ebdb084e84393d7d37a8f1616b664b14
Manjeet <54723481+Manjeets666@users.noreply.github.com>
2022-01-29 16:05:04 +0530

    Update NFTMarketplace.sol

--- NFTMarketplace.sol
+++ NFTMarketplace.sol
@@ -12,7 +12,7 @@ contract PriceConsumerV3 {
      * Aggregator: ETH/USD
      * Address: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
      */
-    constructor() public {
+    constructor()  {
         priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
     }
     
@@ -33,18 +33,41 @@ contract PriceConsumerV3 {
     }
 }
 
-contract nftSale is ERC721 {
+contract nftExchange is ERC721, PriceConsumerV3 {
     event NftBought(address _seller, address _buyer, uint256 _sellingPrice);
-    address public seller;
+    event AuctionEnded(address winner, uint256 amount);
+    event BidSuccess(address bidder, uint256 amount, uint256 _id);
+    event AuctionCreated(address creator, uint256 _id, uint256 _basePrice);
 
+    mapping (uint256 => address payable) public seller;
     mapping (uint256 => uint256) public idToPrice;
+    mapping (uint256 => uint256) public endTime;
+    mapping (uint256 => address) public highestBidder;
+    mapping (uint256 => uint256) public highestBid;
+    mapping (uint256 => uint256) public idToBasePrice;
+    // Mapping from id to mapping from bidCount to user bid data
+    mapping(uint256 => mapping (uint256 => Bid)) public bids;
+    //to store no. of bids for each id
+    mapping (uint256 => uint256) public bidCount;
+    // Bid struct to hold bidder and amount
+    struct Bid {
+        address payable bidder;
+        uint256 amount;
+}
 
-    constructor() ERC721('MyNFT', 'MY') {
-        _mint(msg.sender, 1);
+    constructor() ERC721('', '') {}
+
+    modifier onlySeller(uint256 _id) {
+        require(seller[_id] == msg.sender, "caller is not the seller");
+        _;
     }
-
-    modifier onlySeller() {
-        require(seller == msg.sender, "caller is not the seller");
+    modifier notNftSeller(uint256 _id) {
+        require(msg.sender != seller[_id], "Owner cannot bid on own NFT");
+        _;
+    }
+    //to see if auction is ended or not
+    modifier auctionOngoing(uint256 _id) {
+        require(isAuctionOngoing(_id),"Auction has ended");
         _;
     }
 
@@ -53,11 +76,11 @@ contract nftSale is ERC721 {
         require(_sellingPrice > 0, "Price zero");
         idToPrice[_id] = _sellingPrice;
         transferNftToContract(_id);
-        seller= payable(msg.sender);
+        seller[_id]= payable(msg.sender);
     }
 
-    function cancelNftSale(uint256 _id) external onlySeller{
-        _transfer(address(this),seller, _id);
+    function cancelNftSale(uint256 _id) external onlySeller(_id){
+        _transfer(address(this),seller[_id], _id);
         require(ownerOf(_id) != address(this),"NFT transfer failed");
     }
 
@@ -71,106 +94,66 @@ contract nftSale is ERC721 {
         require(price > 0, "This NFT is not for sale");
         require(msg.value == price, "Incorrect value");
         _transfer(address(this), msg.sender, _id);
-        payable(seller).transfer(msg.value - (msg.value * 25/1000)); // send the ETH to the seller with 2.5% fee
+        seller[_id].transfer(msg.value - (msg.value * 25/1000)); // send the ETH to the seller with 2.5% fee
         idToPrice[_id] = 0;
-        seller = address(0);
-        
-        emit NftBought(seller, msg.sender, msg.value);
+        seller[_id] = payable(address(0));
+
+        emit NftBought(seller[_id], msg.sender, msg.value);
     }
     
-    function changeSellPrice(uint256 _id, uint256 _newPrice) external onlySeller{
+    function changeSellPrice(uint256 _id, uint256 _newPrice) external onlySeller(_id){
         idToPrice[_id] = _newPrice;
-    }
-}
-
-contract nftAuction is ERC721, PriceConsumerV3  {
-    //parametrs
-    uint256 public endTime;
-    uint256 public highestBid;
-    uint256 public bidderCount;
-    address payable public seller;
-    address payable public highestBidder;
-//     struct Bidder {
-//         address payable addr;
-//         uint256 amount;
-// }
-    mapping (uint256 => uint256) public idToBasePrice;
-    //we need mapping to store the bidAmount of every Bidder
-    mapping(address => uint256) public bidders;
-    //to see if auction is ended or not
-
-    modifier onlySeller() {
-        require(seller == msg.sender, "caller is not the seller");
-        _;
-    }
-    modifier notNftSeller(uint256 _id) {
-        require(msg.sender != seller, "Owner cannot bid on own NFT");
-        _;
-    }
-    modifier auctionOngoing(uint256 _id) {
-        require(isAuctionOngoing(_id),"Auction has ended");
-        _;
-    }
-    // modifier isAuctionOver(uint256 _id) {
-    //     require(!isAuctionOngoing(_id),"Auction is not yet over");
-    //     _;
-    // }
-
-    //events
-    event AuctionEnded(address winner, uint256 amount);
-
-    constructor() ERC721('MyNFT', 'MY') {
-        _mint(msg.sender, 1);
     }
 
     function sellAsAuction(uint256 _id, uint256 _basePrice, uint256 _endTime) external {
         require(msg.sender == ownerOf(_id), "Not owner of this NFT");
         require(_basePrice > 0, "Price zero");
-        seller= payable(msg.sender);
-        endTime= block.timestamp + _endTime;
+        seller[_id]= payable(msg.sender);
+        endTime[_id]= block.timestamp + _endTime;
         uint256 ethPrice = ethUSD(_basePrice);
         idToBasePrice[_id] = ethPrice;
         transferNftToContract(_id);
-        
+
+        emit AuctionCreated(msg.sender, _id, _basePrice);   
     }
     
     function makeBid(uint256 _id) external payable notNftSeller(_id) auctionOngoing(_id) {
         require(idToBasePrice[_id] > 0, "This NFT is not in auction");
-        require(msg.value >= idToBasePrice[_id] && msg.value > highestBid,"There is already a higher or equal bid");
-        if(highestBid != 0){
-            bidders[highestBidder] += highestBid;
-        }
-        highestBidder= payable(msg.sender);
-        highestBid= msg.value;
-        // bidderCount++;
-        // bidders[bidderCount]= Bidder(highestBidder,highestBid);
+        require(msg.value >= idToBasePrice[_id] && msg.value > highestBid[_id],"There is already a higher or equal bid");
+        bids[_id][bidCount[_id]].bidder = payable(msg.sender);
+        bids[_id][bidCount[_id]].amount += msg.value;
+        highestBidder[_id]= bids[_id][bidCount[_id]].bidder;
+        highestBid[_id]= bids[_id][bidCount[_id]].amount;
+        bidCount[_id]++;
+
+        emit BidSuccess(msg.sender, msg.value, _id);
     }
 
     function isAuctionOngoing(uint256 _id) internal view returns (bool){
-        return(endTime == 0 || block.timestamp <= endTime );
+        return(block.timestamp <= endTime[_id] );
     }
 
-    function transferNftToContract(uint256 _id) internal {
-        _transfer(ownerOf(_id), address(this), _id);
-        require(ownerOf(_id) == address(this),"NFT transfer failed");
+    function refundBidders(uint256 _id) internal {
+        for(uint8 i = 0; i < bidCount[_id]-1; i++){
+            address payable addr = bids[_id][i].bidder;
+            uint256 amt = bids[_id][i].amount;
+            addr.transfer(amt);
+        }
     }
 
-    // function refundBidders() internal {
-    //     for(uint8 i=0; i < bidderCount-1; i++){
-
-    //     }
-    // }
-
-    function endAuction(uint256 _id) external payable onlySeller auctionOngoing(_id){
-        _transfer(address(this), highestBidder, _id);
-        payable(seller).transfer(highestBid-(highestBid * 25/1000)); ///// send the ETH to the seller with 2.5% fee
+    function endAuction(uint256 _id) external payable onlySeller(_id) auctionOngoing(_id){
+        if(bidCount[_id] > 0){ //to check if there are bidders or not
+            _transfer(address(this), highestBidder[_id], _id);
+            seller[_id].transfer(highestBid[_id]-(highestBid[_id] * 25/1000)); // send the ETH to the seller with 2.5% fee
+            refundBidders(_id);
+        }
+        //reset everything of that nft id
         idToBasePrice[_id] = 0; // not for auction anymore       
-        seller = payable(address(0));
-        endTime=0;
-        highestBidder=payable(address(0));
-        highestBid=0;
+        seller[_id] = payable(address(0));
+        endTime[_id] = 0;
+        highestBidder[_id] = payable(address(0));
+        highestBid[_id] = 0;
 
-        emit AuctionEnded(highestBidder,highestBid);
-
+        emit AuctionEnded(highestBidder[_id],highestBid[_id]);
     }
 }
